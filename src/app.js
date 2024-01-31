fc923252b6e01320cf6ea48e5c2fd151a3c6a2fd
olgagrinko-dev <olgagrinko.87@mail.ru>
2024-01-31 19:31:17 +0300

    swagger

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./controller/task.controller');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/task', route);
-app.use((error, req, res, _next)=> res.send(error.message));
+app.use((error, req, res, _next) => res.send(error.message));
 
 module.exports = app;
