7fe3d5755eb277d108848323fa3a606795660bb8
olgagrinko-dev <olgagrinko.87@mail.ru>
2024-02-05 15:35:35 +0300

    axios

--- client/src/page/Main.tsx
+++ client/src/page/Main.tsx
@@ -1,20 +1,61 @@
 import style from './style.module.scss';
+import axios from 'axios';
+import { useState } from 'react';
 
 export default function Main() {
+    const [inp, setInp] = useState({ title: '', description: '' });
+
+    function changeInput(event: any) {
+        setInp({ ...inp, [event.target.name]: event.target.value });
+    };
+
+    async function isShow() {
+        const result = await axios.post('http://localhost:3000/task', inp);
+        console.log(result);
+    }
 
     return (
         <div className={style.wrapper}>
             <h1>TODO LIST</h1>
-
             <div className={style.inpCreat}>
-                <input placeholder='Create note...'/>
-                <input placeholder='Create description note...'/>
-                <button>CREATE</button>
-            </div>
-            <div>
-                <input type="checkbox" checked></input>
+                <input type="text" name='title' onChange={changeInput} placeholder='Create note...' />
+                <input type="text" name='description' onChange={changeInput} placeholder='Create description note...' />
+                <button onClick={isShow}>CREATE</button>
             </div>
 
+            <div className={style.inpTask1}>
+                <input type="checkbox" ></input>
+                <h2>Note #1</h2>
+                <p>DESCRIPTION...</p>
+                <div className={style.imgMain}>
+                    <div className={style.imgPencil}></div>
+                    <div className={style.imgBasket}></div>
+                </div>
+            </div>
+
+            <div className={style.line}></div>
+
+            <div className={style.inpTask2}>
+                <input type="checkbox"></input>
+                <h2>Note #2</h2>
+                <p>DESCRIPTION...</p>
+                <div className={style.imgMain}>
+                    <div className={style.imgPencil}></div>
+                    <div className={style.imgBasket}></div>
+                </div>
+            </div>
+
+            <div className={style.line}></div>
+
+            <div className={style.inpTask3}>
+                <input type="checkbox"></input>
+                <h2>Note #3</h2>
+                <p>DESCRIPTION...</p>
+                <div className={style.imgMain}>
+                    <div className={style.imgPencil}></div>
+                    <div className={style.imgBasket}></div>
+                </div>
+            </div>
 
         </div>
     )
